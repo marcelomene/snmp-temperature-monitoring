3370c84ccaec521bbb69e5ff93b73dd6e05df300
Marcelo Menezes <marcelo.jorda@hotmail.com>
2019-10-24 21:35:09 -0300

    Conexão com wifi funcionando

--- snmp_temperature_monitoring.ino
+++ snmp_temperature_monitoring.ino
@@ -1,33 +1,52 @@
-#include "DHT.h"
-//https://github.com/adafruit/DHT-sensor-library Biblioteca do DHT11
- 
-#define DHTPIN A1 // pino analógico do sensor
+#include "DHT.h" /* Temperature and humidity sensor lib */
+#include <ESP8266WiFi.h> /* Wi-fi lib */
+#include "Agentuino.h"
+
+/* DHT11 */
+#define DHTPIN 0 // pino analógico do sensor
 #define DHTTYPE DHT11 // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
- 
+
+/* Wi-fi */
+const char *ssid =  "FADERGS";     // replace with your wifi ssid and wpa2 key
+const char *pass =  "";
+
+
+
 void setup() 
 {
   Serial.begin(9600);
   Serial.println("Comunicação serial iniciada...");
+  Serial.println("Iniciando DHT11...");
   dht.begin();
+
+  Serial.println("Iniciando Wi-Fi...");
+  WiFi.begin(ssid, pass); 
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected"); 
 }
  
 void loop() 
 {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  //float h = dht.readHumidity();
+  //float t = dht.readTemperature();
   // testa se retorno é valido, caso contrário algo está errado.
-  if (isnan(t) || isnan(h)) 
-  {
-    Serial.println("Failed to read from DHT");
-  } 
-  else
-  {
-    Serial.print("Umidade: ");
-    Serial.print(h);
-    Serial.print(" %t");
-    Serial.print("Temperatura: ");
-    Serial.print(t);
-    Serial.println(" *C");
-  }
+  //if (0) 
+  //{
+   // Serial.println("Failed to read from DHT");
+  //} 
+  //else
+  //{
+   // Serial.print("Umidade: ");
+   // Serial.print(h);
+    //Serial.print(" %t");
+   // Serial.print("Temperatura: ");
+   // Serial.print(t);
+   // Serial.println(" *C");
+  //}
 }
